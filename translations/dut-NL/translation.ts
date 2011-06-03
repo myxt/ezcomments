b993f336afeb065ebd0de920cf6dd5cd107bb33c
svda <sander@myxt.nl>
2011-06-03 22:52:42 +0200

    Added NL translation.

---
+++ translations/dut-NL/translation.ts
@@ -0,0 +1,484 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="nl_NL">
+<context>
+    <name>comments/comment/menu</name>
+    <message>
+        <source>eZ Comments</source>
+        <translation>eZ Comments</translation>
+    </message>
+    <message>
+        <source>Comments list</source>
+        <translation>Lijst met reacties</translation>
+    </message>
+</context>
+<context>
+    <name>design/admin/parts/user/menu</name>
+    <message>
+        <source>Change the left menu width to small size.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Small</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Medium</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Change the left menu width to large size.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Large</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Change the left menu width to medium size.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>design/ezwebin/user/edit</name>
+    <message>
+        <source>User profile</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Username</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Email</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>My drafts</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>My orders</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>My pending items</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>My notification settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>My wish list</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Edit profile</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Change password</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ezcomments/class/edit</name>
+    <message>
+        <source>eZ Comments warning: the reCAPTCHA key is not set up properly.</source>
+        <translation>eZ Comments waarschuwing: de reCAPTCHA key is niet goed ingesteld.</translation>
+    </message>
+    <message>
+        <source>Please get a reCAPTCHA key from &lt;a href=&quot;https://admin.recaptcha.net/recaptcha/createsite&quot; target=&quot;_blank&quot;&gt;https://admin.recaptcha.net/recaptcha/createsite&lt;/a&gt; then set it up in eZ Comments, or disable CAPTCHA feature.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>For more details please visit &lt;a href=&quot;http://projects.ez.no/ezcomments&quot; target=&quot;_blank&quot;&gt;http://projects.ez.no/ezcomments&lt;/a&gt;.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ezcomments/comment/action</name>
+    <message>
+        <source>Back</source>
+        <translation>Terug</translation>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translation>Verwijderen</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>Annuleren</translation>
+    </message>
+    <message>
+        <source>Update comment</source>
+        <translation>Reactie aanpassen</translation>
+    </message>
+</context>
+<context>
+    <name>ezcomments/comment/activate</name>
+    <message>
+        <source>The subscription is activated!</source>
+        <translation>Het abonnement is geactiveerd!</translation>
+    </message>
+    <message>
+        <source>Go to settings</source>
+        <translation>Naar de instellingen</translation>
+    </message>
+</context>
+<context>
+    <name>ezcomments/comment/activationnotification</name>
+    <message>
+        <source>Dear %email,</source>
+        <translation>Beste %email,</translation>
+    </message>
+    <message>
+        <source>You have subscribed to comment update on content. Before you start the service, you need to confirm the subscription.</source>
+        <translation>Je hebt je opgegeven voor de reactie-update. Voordat je van deze dienst gebruik kunt maken, moet je het abonnement bevestigen.</translation>
+    </message>
+    <message>
+        <source>Please click the link to activate.</source>
+        <translation>Klik op de link om te activeren.</translation>
+    </message>
+    <message>
+        <source>The activation will expire after %expiry_days days.</source>
+        <translation>De activatielink verloopt na %expiry_days dagen.</translation>
+    </message>
+    <message>
+        <source>The activation will expire after %expiry_hours hours.</source>
+        <translation>De activatielink verloopt na %expiry_hours uur.</translation>
+    </message>
+    <message>
+        <source>If you do not want to receive the subscription email, ignore this email.</source>
+        <translation>Als je het abonnement niet wilt kun je deze mail negeren.</translation>
+    </message>
+    <message>
+        <source>You can go to your setting page to manage your subscription.</source>
+        <translation>Op je profielpagina kun je je abonnement beheren.</translation>
+    </message>
+    <message>
+        <source>Activate your subscription</source>
+        <translation>Activeer je abonnement</translation>
+    </message>
+    <message>
+        <source>Hi,</source>
+        <translation>Hoi,</translation>
+    </message>
+    <message>
+        <source>posted a new comment on</source>
+        <translation>heeft een nieuwe reactie geplaatst bij</translation>
+    </message>
+    <message>
+        <source>Comment summary</source>
+        <translation>Samenvatting van de reactie</translation>
+    </message>
+    <message>
+        <source>To reply the content, please visit</source>
+        <translation>Ga naar de volgende pagina om op de inhoud te reageren</translation>
+    </message>
+    <message>
+        <source>For setting your subscription, please visit</source>
+        <translation>Om je abonnement te beheren, ga naar</translation>
+    </message>
+    <message>
+        <source>(You need to login)</source>
+        <translation>(Je moet hiervoor inloggen)</translation>
+    </message>
+    <message>
+        <source>There are updates from</source>
+        <translation>Er zijn updates van</translation>
+    </message>
+</context>
+<context>
+    <name>ezcomments/comment/add</name>
+    <message>
+        <source>Your comment has been posted.</source>
+        <translation>Reactie geplaatst.</translation>
+    </message>
+    <message>
+        <source>reCAPTCHA API key missing.</source>
+        <translation>reCAPTCHA API key mist.</translation>
+    </message>
+</context>
+<context>
+    <name>ezcomments/comment/add/form</name>
+    <message>
+        <source>Post comment</source>
+        <translation>Reactie plaatsen</translation>
+    </message>
+    <message>
+        <source>Title:</source>
+        <translation>Titel:</translation>
+    </message>
+    <message>
+        <source>Name:</source>
+        <translation>Naam:</translation>
+    </message>
+    <message>
+        <source>Website:</source>
+        <translation>Website:</translation>
+    </message>
+    <message>
+        <source>Email:</source>
+        <translation>E-mailadres:</translation>
+    </message>
+    <message>
+        <source>(The email address will not be shown)</source>
+        <translation>(het e-mailadres is niet zichtbaar op de site)</translation>
+    </message>
+    <message>
+        <source>Content:</source>
+        <translation>Reactie:</translation>
+    </message>
+    <message>
+        <source>Notify me of new comments</source>
+        <translation>Breng me op de hoogte bij nieuwe reacties</translation>
+    </message>
+    <message>
+        <source>Security text:</source>
+        <translation>Beveiligingscode:</translation>
+    </message>
+    <message>
+        <source>Enter both words below, with or without a space.</source>
+        <translation>Voer de onderstaande woorden in.</translation>
+    </message>
+    <message>
+        <source>The letters are not case-sensitive.</source>
+        <translation>De woorden zijn niet hoofdlettergevoelig.</translation>
+    </message>
+    <message>
+        <source>Can&apos;t read this?</source>
+        <translation>Niet leesbaar?</translation>
+    </message>
+    <message>
+        <source>Try another</source>
+        <translation>Probeer een andere</translation>
+    </message>
+    <message>
+        <source>Remember me</source>
+        <translation>Gegevens onthouden</translation>
+    </message>
+    <message>
+        <source>Add comment</source>
+        <translation>Reactie toevoegen</translation>
+    </message>
+    <message>
+        <source>Reset form</source>
+        <translation>Formulier wissen</translation>
+    </message>
+    <message>
+        <source>Notified of new comments</source>
+        <translation>Op de hoogte gebracht bij nieuwe reacties</translation>
+    </message>
+</context>
+<context>
+    <name>ezcomments/comment/delete</name>
+    <message>
+        <source>Delete comment?</source>
+        <translation>Reactie verwijderen?</translation>
+    </message>
+</context>
+<context>
+    <name>ezcomments/comment/edit</name>
+    <message>
+        <source>Edit comment</source>
+        <translation>Reactie wijzigen</translation>
+    </message>
+    <message>
+        <source>You don&apos;t have access to edit.</source>
+        <translation>Geen toegang om te wijzigen.</translation>
+    </message>
+</context>
+<context>
+    <name>ezcomments/comment/list</name>
+    <message>
+        <source>Comments list</source>
+        <translation>Lijst met reacties</translation>
+    </message>
+    <message>
+        <source>Select all comments</source>
+        <translation>Alle reacties selecteren</translation>
+    </message>
+    <message>
+        <source>Comment</source>
+        <translation>Reactie</translation>
+    </message>
+    <message>
+        <source>Node</source>
+        <translation>Node</translation>
+    </message>
+    <message>
+        <source>Modified</source>
+        <translation>Gewijzigd</translation>
+    </message>
+    <message>
+        <source>IP address</source>
+        <translation>IP adres</translation>
+    </message>
+    <message>
+        <source>Edit comment</source>
+        <translation>Reactie wijzigen</translation>
+    </message>
+    <message>
+        <source>Remove selected</source>
+        <translation>Selectie verwijderen</translation>
+    </message>
+</context>
+<context>
+    <name>ezcomments/comment/notification</name>
+    <message>
+        <source>New comment</source>
+        <translation>Nieuwe reactie</translation>
+    </message>
+</context>
+<context>
+    <name>ezcomments/comment/removecomments</name>
+    <message>
+        <source>Confirm comments removal</source>
+        <translation>Verwijdering van reacties bevestigen</translation>
+    </message>
+    <message>
+        <source>Are you sure you want to remove selected comments?</source>
+        <translation>Weet u zeker dat u de geselecteerde reacties wilt verwijderen?</translation>
+    </message>
+    <message>
+        <source>OK</source>
+        <translation>OK</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>Annuleren</translation>
+    </message>
+</context>
+<context>
+    <name>ezcomments/comment/setting</name>
+    <message>
+        <source>The subscriber doesn&apos;t exist.</source>
+        <translation>Abonnee niet gevonden.</translation>
+    </message>
+    <message>
+        <source>You are not subscribing to any content.</source>
+        <translation>Geen inhoud geselecteerd.</translation>
+    </message>
+    <message>
+        <source>You have updated comment settings.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>OK</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Select</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Content</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Subscription started</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Subscribed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Save</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Reset</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ezcomments/comment/view</name>
+    <message>
+        <source>wrote:</source>
+        <translation>schreef:</translation>
+    </message>
+    <message>
+        <source>Edit</source>
+        <translation>Wijzigen</translation>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translation>Verwijderen</translation>
+    </message>
+    <message>
+        <source>There is no comment.</source>
+        <translation>Geen reacties.</translation>
+    </message>
+    <message>
+        <source>You don&apos;t have access to view comment.</source>
+        <translation>Geen toegang voor het bekijken van reacties.</translation>
+    </message>
+    <message>
+        <source>You don&apos;t have access to post comment.</source>
+        <translation>Geen toegang voor het plaatsen van reacties.</translation>
+    </message>
+    <message>
+        <source>View all %total_count comments</source>
+        <translation>Alle %total_count reacties bekijken</translation>
+    </message>
+    <message>
+        <source>Total %total_count comments</source>
+        <translation>%total_count reacties</translation>
+    </message>
+</context>
+<context>
+    <name>ezcomments/comment/view/action</name>
+    <message>
+        <source>Back</source>
+        <translation>Terug</translation>
+    </message>
+</context>
+<context>
+    <name>ezcomments/comment/view/page</name>
+    <message>
+        <source>%current_page/%total_page</source>
+        <translation>%current_page/%total_page</translation>
+    </message>
+    <message>
+        <source>Previous page</source>
+        <translation>Vorige pagina</translation>
+    </message>
+    <message>
+        <source>Next page</source>
+        <translation>Volgende pagina</translation>
+    </message>
+    <message>
+        <source>Total comments:</source>
+        <translation>Totaal aantal reacties:</translation>
+    </message>
+    <message>
+        <source>Pages:</source>
+        <translation>Pagina&apos;s:</translation>
+    </message>
+</context>
+<context>
+    <name>ezcomments/datatype/edit</name>
+    <message>
+        <source>Show comments</source>
+        <translation>Reacties tonen</translation>
+    </message>
+    <message>
+        <source>Enable commenting</source>
+        <translation>Reageren activeren</translation>
+    </message>
+</context>
+<context>
+    <name>ezcomments/ezwebin/user/edit</name>
+    <message>
+        <source>My comment settings</source>
+        <translation>Mijn reactie-instellingen</translation>
+    </message>
+</context>
+</TS>
